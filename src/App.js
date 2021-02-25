cf878bd8f50a6a1475f7a5399af5985133d5b204
Diana Walsh <diwalsh@hubspot.local>
2021-02-25 21:03:20 +0000

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Readings from "./components/readings";
+import Users from "./components/users";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Readings />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/users" component={Users} />
+            <Route path="/readings" component={Readings} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/users" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
