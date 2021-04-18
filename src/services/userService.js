d8a677c63ae7d5a06b42bced4207cbd50110f072
DianaWalsh19 <69216021+DianaWalsh19@users.noreply.github.com>
2021-04-18 17:37:18 +0100

    Fixing CRUD operation on User Service

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,9 @@ import http from "./httpService";
 const apiEndpoint = "/users";
 
 export function register(user) {
-  return http.get(apiEndpoint + { email: user.email, password: user.password });
+  return http.post(
+    apiEndpoint + { email: user.email, password: user.password }
+  );
 }
 
 export function getUsers() {
